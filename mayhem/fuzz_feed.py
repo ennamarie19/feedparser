bec3fc0325849820dcee8448667e1b8bd9578eed
ennamarie19 <dallmeyerm@att.net>
2022-10-27 17:30:38 -0500

    Fixed testsuite

--- mayhem/fuzz_feed.py
+++ mayhem/fuzz_feed.py
@@ -12,16 +12,18 @@ with atheris.instrument_imports():
 def TestOneInput(data):
     try:
         fdp = atheris.FuzzedDataProvider(data)
-        file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
-        temp_file = tempfile.NamedTemporaryFile()
-        temp_file.write(file_data)
-        temp_file.flush()
-        feedparser.parse(temp_file.name)
+        file_data = fdp.ConsumeUnicodeNoSurrogates(atheris.ALL_REMAINING)
+        f = io.StringIO(file_data)
+        #temp_file = tempfile.NamedTemporaryFile()
+        #temp_file.write(file_data)
+        #temp_file.flush()
+        feedparser.parse(f)
 
     except IndexError:
         if random.randint(0, 100) == 50:
             raise
 
+    #temp_file.close()
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
