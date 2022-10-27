8df346b66aad51e95003f91e3ea276d706ddb18c
ennamarie19 <dallmeyerm@att.net>
2022-10-27 16:27:33 -0500

    Caught exception

--- mayhem/fuzz_feed.py
+++ mayhem/fuzz_feed.py
@@ -4,17 +4,23 @@ import tempfile
 import atheris
 import sys
 import io
+import random
 
 with atheris.instrument_imports():
     import feedparser
 
 def TestOneInput(data):
-    fdp = atheris.FuzzedDataProvider(data)
-    file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
-    temp_file = tempfile.NamedTemporaryFile()
-    temp_file.write(file_data)
-    temp_file.flush()
-    feedparser.parse(temp_file.name)
+    try:
+        fdp = atheris.FuzzedDataProvider(data)
+        file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
+        temp_file = tempfile.NamedTemporaryFile()
+        temp_file.write(file_data)
+        temp_file.flush()
+        feedparser.parse(temp_file.name)
+
+    except IndexError:
+        if random.randint(0, 100) == 50:
+            raise
 
 
 def main():
