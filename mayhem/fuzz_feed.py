7051ff6e233c06904b2c587194b9e9998ed4ba80
ennamarie19 <dallmeyerm@att.net>
2022-10-27 15:52:57 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_feed.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+import tempfile
+
+import atheris
+import sys
+import io
+
+with atheris.instrument_imports():
+    import feedparser
+
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+    file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
+    temp_file = tempfile.NamedTemporaryFile()
+    temp_file.write(file_data)
+    temp_file.flush()
+    temp_file.name = "test" + ".xml"
+    feedparser.parse(temp_file)
+
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
