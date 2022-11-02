679c011714e876a37427663f71a39821ec5870aa
Bailey Capuano <bcapuano@asu.edu>
2022-11-01 22:51:34 -0400

    Reset to use strings

--- mayhem/fuzz_feed.py
+++ mayhem/fuzz_feed.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 import tempfile
 
 import atheris
@@ -13,11 +13,7 @@ def TestOneInput(data):
     try:
         fdp = atheris.FuzzedDataProvider(data)
         file_data = fdp.ConsumeUnicodeNoSurrogates(atheris.ALL_REMAINING)
-        f = io.StringIO(file_data)
-        #temp_file = tempfile.NamedTemporaryFile()
-        #temp_file.write(file_data)
-        #temp_file.flush()
-        feedparser.parse(f)
+        feedparser.parse(file_data)
 
     except IndexError:
         if random.randint(0, 100) == 50:
